1c5ab326949d876a4bd8c8fbe03221c3f1179784
Austin Whitbeck <AustinWhitbeckArt@gmail.com>
2023-11-24 09:04:09 -0500

    make a note

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* Note add prettier file */}
       <ThemeRegistry>      <body className={inter.className}>{children}</body></ThemeRegistry>
     </html>
   )
