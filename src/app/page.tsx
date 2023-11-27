87b2298b90f0c3fcb7c092c161f5c9ce8448f334
Austin <austinwhitbeckart@gmail.com>
2023-11-26 22:27:44 -0500

    home page reworked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import { Stack, Typography } from "@mui/material"
-import MainIcon from "../images/icon_version_1.png";
+import MainIcon from "../images/icon_version_3.png";
 import Image from "next/image";
 
 const HomePage = () => {
   return (
-    <Stack>
-      <Typography variant="h1">Androidgynous</Typography>
-      <Typography variant="h3" component="h2">The future is not defined by the binary</Typography>
-      <Image src={MainIcon} alt="Androidgynous Logo" width={500} height={400} />
+    <Stack sx={{ alignItems: 'center'}}>
+      <Typography variant="h1" sx={{ color: 'navy'}}>Androidgynous</Typography>
+      <Typography variant="subtitle2" component="h2" sx={{ color: 'pink'}}>The future is not defined by the binary</Typography>
+      <Image src={MainIcon} alt="Androidgynous Logo" width={500} height={500} style={{ borderRadius: '50%', border: '2px solid purple'}}/>
     </Stack>
   )
 }
